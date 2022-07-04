176557d610396aa57a7c4905ffd275c247adceed
cesarApr <cesareaprilend76@gmail.com>
2022-07-04 16:17:26 +0700

    Revised

--- routes/api.php
+++ routes/api.php
@@ -23,5 +23,9 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['auth:sanctum'])->group(function (){
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware(['auth:sanctum'])->group(function (){
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('/todos', TodoController::class);
 });
